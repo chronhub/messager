53836dfba3569d3ada9cd876cd6e0905037101a9
steph bug <chronhubgit@gmail.com>
2022-07-01 15:12:30 +0200

    manager and provider

---
+++ src/MessageServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronhub\Messager;
+
+use Illuminate\Support\ServiceProvider;
+use Chronhub\Messager\Support\Clock\Clock;
+use Chronhub\Messager\Message\Alias\MessageAlias;
+use Chronhub\Messager\Support\Facade\AliasMessage;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Chronhub\Messager\Message\Factory\MessageFactory;
+use Chronhub\Messager\Message\Serializer\MessageSerializer;
+
+final class MessageServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    private string $reporterPath = __DIR__.'/../config/messager.php';
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([$this->reporterPath => config_path('messager.php')]);
+        }
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom($this->reporterPath, 'reporter');
+
+        $this->app->singleton(MessagerManager::class);
+        //$this->app->alias(ReporterManager::class, Report::SERVICE_NAME);
+        //$this->app->bind(Publish::SERVICE_NAME, Publisher::class);
+
+        $config = config('messager');
+
+        $this->app->bind(Clock::class, $config['clock']);
+
+        $message = $config['messaging'];
+
+        $this->app->bind(MessageFactory::class, $message['factory']);
+        $this->app->bind(MessageSerializer::class, $message['serializer']);
+
+        $this->app->bind(MessageAlias::class, $message['alias']);
+        $this->app->alias(MessageAlias::class, AliasMessage::SERVICE_NAME);
+    }
+
+    public function provides(): array
+    {
+        return [
+            MessagerManager::class,
+            MessageFactory::class,
+            MessageSerializer::class,
+            MessageAlias::class,
+            AliasMessage::SERVICE_NAME,
+        ];
+    }
+}
