98622cdd07161736148e17311827ba26da69704f
steph bug <chronhubgit@gmail.com>
2022-07-12 14:27:24 +0200

    fix

--- src/Message/Serializer/GenericContentSerializer.php
+++ src/Message/Serializer/GenericContentSerializer.php
@@ -6,7 +6,6 @@ namespace Chronhub\Messager\Message\Serializer;
 
 use Chronhub\Messager\Message\Domain;
 use Chronhub\Messager\Message\Header;
-use Chronhub\Messager\Message\DomainEvent;
 use Chronhub\Messager\Exceptions\RuntimeException;
 use Chronhub\Messager\Support\Aggregate\AggregateChanged;
 
@@ -17,7 +16,7 @@ final class GenericContentSerializer
         return $event->toContent();
     }
 
-    public function unserialize(string $source, array $payload): DomainEvent
+    public function unserialize(string $source, array $payload): Domain
     {
         if (is_subclass_of($source, AggregateChanged::class)) {
             $aggregateId = $payload['headers'][Header::AGGREGATE_ID->value];
@@ -25,7 +24,7 @@ final class GenericContentSerializer
             return $source::occur($aggregateId, $payload['content']);
         }
 
-        if (is_subclass_of($source, DomainEvent::class)) {
+        if (is_subclass_of($source, Domain::class)) {
             return $source::fromContent($payload['content']);
         }
 
